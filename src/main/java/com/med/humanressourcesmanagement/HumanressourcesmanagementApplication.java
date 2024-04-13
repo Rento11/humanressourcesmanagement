2fc4168e273d30d88f0166fd9e03016f65d70e46
maybe <maybeidkwhoami@outlook.fr>
2024-04-13 14:35:58 +0100

    PROJECT INITIALIZATION

---
+++ src/main/java/com/med/humanressourcesmanagement/HumanressourcesmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.med.humanressourcesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanressourcesmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanressourcesmanagementApplication.class, args);
+    }
+
+}
